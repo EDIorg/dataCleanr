594045d0ac0eafd2dea5b39cb6c90cc75556f30e
clnsmth <colin.smith@wisc.edu>
2019-02-05 09:57:44 -0600

    Move example data to /data

---
+++ R/data_iso8601.R
@@ -0,0 +1,12 @@
+#' Example data for test driving iso8601_* functions
+#'
+#' A dataset containing dates and times of different formats.
+#'
+#' @format A data frame with 31 rows and 4 columns:
+#' \describe{
+#'   \item{date}{Time series incrementing by day}
+#'   \item{date_2_formats}{Time series incrementing by day in two formats}
+#'   \item{datetime}{Dates and times incrementing by hour}
+#'   \item{datetime_mess}{Single date and time represented in many formats}
+#' }
+"data_iso8601"
