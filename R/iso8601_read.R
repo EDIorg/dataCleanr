78b8a49565f57495984f79f90a3758845a06770e
clnsmth <colin.smith@wisc.edu>
2019-02-10 20:04:17 -0600

    Update methods and documentation
    Constrain allowed inputs to date and time, date, or time only data (i.e. not a mix of these 3 types).

--- R/iso8601_read.R
+++ R/iso8601_read.R
@@ -1,25 +1,25 @@
-#' Read ISO 8601 formatted strings into POSIXct, POSIXt
+#' Read ISO 8601 strings into POSIXct, POSIXt
 #'
 #' @description
-#'     Read ISO 8601 formatted dates and times into POSIXct, POSIXt.
+#'     Read ISO 8601 strings into POSIXct, POSIXt.
 #'
 #' @details
-#'     `iso8601_read` provides a lightweight option for reading ISO 8601 data 
-#'     in the POSIXct POXIXt class. This function uses regular expressions to 
-#'     extract the orders and time zone offset arguments and then passess the 
-#'     info to `lubridate::parse_date_time` for parsing. Use 
-#'     `lubridate::parse_date_time` for more options on handling time zone 
-#'     offsets and locales.
+#'     `iso8601_read` provides a lightweight option for reading ISO 8601 
+#'     strings into the POSIXct POXIXt class. This function uses regular 
+#'     expressions to extract the orders and time zone offset arguments and 
+#'     passess this info to `lubridate::parse_date_time` for parsing.
 #'
 #' @usage iso8601_read(x)
 #'
 #' @param x
-#'     (character) A vector of ISO 8601 date times created with 
-#'     `dataCleanr::iso8601_convert`.
+#'     (character) A vector of ISO 8601 strings created with `iso8601_convert`.
+#'     Vector contents must be one of dates and times, dates, or times. A mix 
+#'     of more than one type is not supported. A mix of input precision is 
+#'     supported for date and time data.
 #'
 #' @return
-#'     (POSIXct, POSIXt) representation of date times, unless the format
-#'     is 'YYYY', in which case integer class data is returned.
+#'     (POSIXct, POSIXt) A vector of POSIXct POSIXt, unless the format is 
+#'     'YYYY', in which case integer class data is returned.
 #'
 #' @examples 
 #' # Read date time strings
@@ -29,6 +29,9 @@
 #' # Read date time strings with specified time zone offset
 #' datetimes <- iso8601_read(c('2012-05-01T13:23:00-03', '2012-05-02T13:23:00-03'))
 #' class(datetimes)
+#' 
+#' # Multiple time zone offsets are not supported
+#' datetimes <- iso8601_read(c('2012-05-01T13:23:00-03', '2012-05-02T13:23:00-05'))
 #'  
 #'
 #' @export
@@ -41,19 +44,27 @@ iso8601_read <- function(x){
   if (missing(x)){
     stop('Input argument "x" is missing!')
   }
+  
+  vfr <- validate_format_rules(x)
+  
   x <- x[!is.na(x)]
   
-  # Separate timezones if present ---------------------------------------------
+  # Separate timezone(s) if present -------------------------------------------
   
   if (sum(stringr::str_detect(x, 'T')) > 0){
     x_sample <- x[stringr::str_detect(x, 'T')]
-    x_sample <- stringr::str_remove_all(x_sample[1], pattern = '.+[T]')
-    if ((sum(stringr::str_detect(x_sample, '\\+')) > 0)){
-      tz <- stringr::str_extract(x_sample, pattern = '[\\+].+')
+    x_sample <- stringr::str_remove_all(x_sample, pattern = '.+[T]')
+    if ((sum(stringr::str_detect(x_sample, '(\\+)|(\\-)')) > 0)){
+      tz <- stringr::str_extract(x_sample, pattern = '([\\+].+)|([\\-].+)')
       x <- substr(x, start = 1, stop = (nchar(x)-3))
-    } else if ((sum(stringr::str_detect(x_sample, '\\-')) > 0)){
-      tz <- stringr::str_extract(x_sample, pattern = '[\\-].+')
-      x <- substr(x, start = 1, stop = (nchar(x)-3))
+      if (length(unique(tz)) > 1){
+        stop(
+          paste(
+            'Input "x" contains more than one time zone. iso8601_read() does',
+            'not support data with multiple time zones.'
+          )
+        )
+      }
     } else {
       tz <- NULL
     }
@@ -68,42 +79,43 @@ iso8601_read <- function(x){
     sep = '\t', 
     header = TRUE, 
     as.is = TRUE
-    )
+  )
   
   tz_name <- onames$name[onames$offset == as.numeric(tz)] # make character
 
   # Parse datetime strings ----------------------------------------------------
 
   if (!is.null(tz)){
+    
     x_converted <- lubridate::parse_date_time(
       x = x,
       orders = 'ymd_HMS',
-      truncated = 3, 
+      truncated = 3,
       tz = tz_name
     )
+    
   } else {
     
-    format_str <- iso8601_get_format_string(x)
+    format_str <- suppressWarnings(
+      iso8601_get_format_string(x)
+    )
     
-    if (format_str == 'YYYY-MM-DDThh:mm:ss'){
-      
-      x_converted <- lubridate::parse_date_time(
-        x = x,
-        orders = 'ymd_HMS'
+    message(
+      paste0(
+        'Parsing data with format specifier "',
+        format_str,
+        '"'
       )
-      
-    } else if (format_str == 'YYYY-MM-DDThh:mm'){
-      
-      x_converted <- lubridate::parse_date_time(
-        x = x,
-        orders = 'ymd_HM'
-      )
-      
-    } else if (format_str == 'YYYY-MM-DDThh'){
+    )
+    
+    if ((format_str == 'YYYY-MM-DDThh:mm:ss') |
+        (format_str == 'YYYY-MM-DDThh:mm') |
+        (format_str == 'YYYY-MM-DDThh')){
       
       x_converted <- lubridate::parse_date_time(
         x = x,
-        orders = 'ymd_H'
+        orders = 'ymd_HMS',
+        truncated = 3
       )
       
     } else if (format_str == 'YYYY-MM-DD'){
@@ -120,10 +132,9 @@ iso8601_read <- function(x){
     } else {
       
       x_converted <- NULL
-      warning('Time only data is not yet supported. Please use lubridate functions to handle these data.')
+      stop('Time only data is not yet supported.')
       
     }
-    
     
   }
 
